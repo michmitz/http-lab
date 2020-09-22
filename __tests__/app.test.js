ba9cc3331e3cf099e72a5b310eacf610fd46e578
Michelle Stermitz <stermitzm@gmail.com>
2020-09-21 17:21:45 -0700

    Added /, /echo, /red paths with testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,29 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-    
+  it('returns the word hi in plain text via GET', async() => {
+    const response = await request(app)
+      .get('/');
+
+    expect(response.text)
+      .toEqual('hi');
+  });
+
+  it('returns the word hi in plain text via POST', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send({ body: 'hi', contentType: 'text/html', status: '200 OK' });
+
+    expect(response.text)
+      .toEqual('hi');
+  });
+
+  it('returns html with an h1 and the word red', async() => {
+    const response = await request(app)
+      .get('/red');
+
+    expect(response.text)
+      .toEqual('<html><body><h1>red</h1></body></html>');
+  });
 
 });
